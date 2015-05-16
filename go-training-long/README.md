af4398210f61b3ccc8b34d28433a21746e21a3db
Pieter Droogendijk <pieter@binky.org.uk>
2015-05-16 23:41:35 +0200

    Copy go-training to go-training-long

---
+++ go-training-long/README.md
@@ -0,0 +1,7 @@
+go slides
+=========
+
+Presentation slides
+
+[Go training (long)](http://go-talks.appspot.com/github.com/PieterD/slides/go-training-long/go-training.slide)
+
---
+++ go-training-long/hello_world/code.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/PieterD/slides/reverse"
+)
+
+func main() {
+	greeting := "!dlrow ,olleH"
+	fmt.Println(reverse.Reverse(greeting))
+}
